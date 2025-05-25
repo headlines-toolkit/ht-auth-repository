64dfd9cb78fd893d259e22499984c88975ab6e80
fulleni <fulleni@hotmail.com>
2025-05-25 06:24:17 +0100

    chore: init

---
+++ test/src/ht_auth_repository_test.dart
@@ -0,0 +1,11 @@
+// ignore_for_file: prefer_const_constructors
+import 'package:ht_auth_repository/ht_auth_repository.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('HtAuthRepository', () {
+    test('can be instantiated', () {
+      expect(HtAuthRepository(), isNotNull);
+    });
+  });
+}
